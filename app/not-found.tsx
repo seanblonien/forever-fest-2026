ce78f74a9e91ceff7d250acf8672c437e762350d
Sean Blonien <seanb2016@gmail.com>
2025-08-29 23:54:55 -0500

    Added Our Story summary, Added Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/shared/PageLayout';
+import {NotFoundPage} from '@/components/pages';
+
+export default function NotFound() {
+  return (
+    <PageLayout>
+      <NotFoundPage />
+    </PageLayout>
+  );
+}
